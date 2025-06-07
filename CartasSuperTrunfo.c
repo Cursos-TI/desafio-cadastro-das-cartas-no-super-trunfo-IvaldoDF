fbd570f85aabc5d929be02a6d30c2e12f3a909c8
IvaldoDF <ivaldo.dantas@ufpe.br>
2025-06-07 19:50:49 +0000

    Comparando as Cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 // Desafio Super Trunfo - Países
-// Tema 2 - Super Poderes das Cartas
+// Tema 3 - Comparando as Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
 // Siga os comentários para implementar cada parte do desafio.
 
@@ -9,12 +9,12 @@ int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
-    // Declaraççao das Variáveis para a Carta 01
+    // Declaração das Variáveis para a Carta 01
 
     char estado1[01]; // declaração da variável tipo char com nome estado1
     char codigo1[03]; // declaração da variável tipo char com nome codigo1
     char cidade1[30]; // declaração da variável tipo char com nome cidade1
-    int populacao1; // declaração da variável tipo int com nome populacao1
+    unsigned long int populacao1; // declaração da variável tipo int com nome populacao1
     float area1; // declaração da variável tipo float com nome area1
     float pib1; // declaração da variável tipo float com nome pib1
     int pontosturisticos1; // declaração da variável tipo int com nome pontosturisticos1
@@ -23,12 +23,15 @@ int main() {
     float densidadepop1 = populacao1/area1; 
 	float pibcapita1 = pib1/populacao1;
 
+    // Declaração Super_poderes para a carta 01
+    unsigned long int superpoder1 = (populacao1 + area1 + pib1 + pontosturisticos1 + pibcapita1 + 1/densidadepop1);
+    
     // Declaração das Variáveis para a Carta 2
 
     char estado2[01]; // declaração da variável tipo char com nome estado2
     char codigo2[03]; // declaração da variável tipo char com nome codigo2
     char cidade2[30]; // declaração da variável tipo char com nome cidade2
-    int populacao2; // declaração da variável tipo int com nome populacao2
+    unsigned long int populacao2; // declaração da variável tipo int com nome populacao2
     float area2; // declaração da variável tipo float com nome area2
     float pib2; // declaração da variável tipo float com nome pib2
     int pontosturisticos2; // declaração da variável tipo int com nome pontosturisticos2
@@ -37,6 +40,9 @@ int main() {
     float densidadepop2 = populacao2/area2;
 	float pibcapita2 = pib2/populacao2;
 
+    // Declaração Super_poderes para a carta 02
+    unsigned long int superpoder2 = (populacao2 + area2 + pib2 + pontosturisticos2 +pibcapita2 + 1/densidadepop2);
+    
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
@@ -45,16 +51,16 @@ int main() {
     printf("Carta 01:\n");
 
     printf("Digite o nome do Estado:\n ");
-    scanf(" %s", &estado1);
+    scanf("%s", &estado1);
 
     printf("Digite o Código da Cidade:\n ");
     scanf("%s", &codigo1);
 
     printf("Digite o nome da Cidade:\n ");
-    scanf(" %s", &cidade1);
+    scanf("%s", &cidade1);
 
     printf("Digite a quantidade da População:\n ");
-    scanf("%d", &populacao1);
+    scanf("%lu", &populacao1);
 
     printf("Digite a Área em  km² da Cidade:\n ");
     scanf("%f", &area1);
@@ -71,16 +77,16 @@ int main() {
     printf("Carta 02:\n");
 
     printf("Digite o nome do Estado:\n ");
-    scanf(" %s", &estado2);
+    scanf("%s", &estado2);
 
     printf("Digite o Código da Cidade:\n ");
     scanf("%s", &codigo2);
 
     printf("Digite o nome da Cidade:\n ");
-    scanf(" %s", &cidade2);
+    scanf("%s", &cidade2);
 
     printf("Digite a quantidade da População:\n ");
-    scanf("%d", &populacao2);
+    scanf("%lu", &populacao2);
 
     printf("Digite a Área em  km² da Cidade:\n ");
     scanf("%f", &area2);
@@ -105,7 +111,7 @@ int main() {
 
     printf("Nome da Cidade: %s\n", cidade1);
 
-    printf("População: %d\n", populacao1);
+    printf("População: %lu\n", populacao1);
 
     printf("Área da Cidade: %.2f km²\n", area1);
 
@@ -117,6 +123,9 @@ int main() {
     printf("A Densidade Populacional é: %.2f hab/km²\n", densidadepop1);
     printf("O PIB per Capita é: %.2f reias\n", pibcapita1);
 
+    // Exibição do Super Poder da carta 01
+    printf("superpoder1:%lu\n", superpoder1);
+
     // Exibição dos dados para a Carta 02
 
     printf("\n--- Carta 02 ---\n");
@@ -127,7 +136,7 @@ int main() {
 
     printf("Nome da Cidade: %s\n", cidade2);
 
-    printf("População: %d\n", populacao2);
+    printf("População: %lu\n", populacao2);
 
     printf("Área da Cidade: %.2f km²\n", area2);
 
@@ -138,6 +147,20 @@ int main() {
     // exibição das novas variáveis para a carta 02
     printf("A Densidade Populacional é: %.2f hab/km²\n", densidadepop2);
 	printf("O PIB per Capita é: %.2f reais\n", pibcapita2);
+    
+    // Exibição do Super Poder da carta 02
+    printf("superpoder2:%lu\n", superpoder2);
+    
+    
+    // Realização da comparação entre a carta 01 com a carta 02
 
+	printf("População: Carta 1 venceu %lu\n", populacao1 > populacao2);
+	printf("Área: Carta 1 venceu %d\n", area1 > area2);
+	printf("PIB: Carta 1 venceu %d\n", pib1 > pib2);
+	printf("Pontos Turisticos: Carta 1 venceu %d\n", pontosturisticos1 > pontosturisticos2);
+	printf("Densidade Populacional: Carta 2 venceu %.2f\n", densidadepop1 < densidadepop2);
+	printf("PIB per Capita : Carta 1 venceu %.2f\n", pibcapita1 > pibcapita2);
+	printf("Super Poder: Carta 1 venceu %lu\n", superpoder1 > superpoder2);
+   
     return 0;
 }
